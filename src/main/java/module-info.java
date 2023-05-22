32bb2dc01eb0ffbe6dc9ffd1cb817d863c1c8865
Trevor Miller <trevormiller@Trevors-Air.home>
2023-05-22 11:01:48 -0400

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.financemanager {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.financemanager to javafx.fxml;
+    exports com.example.financemanager;
+}
